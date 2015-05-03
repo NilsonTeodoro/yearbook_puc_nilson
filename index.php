e2f14efef79be82f95dc88e31b11e2e446ac6bce
NilsonTeodoro <nilsonkz22@gmail.com>
2015-05-03 17:18:01 -0300

    Retirando require_once
    Retirando require_once

--- index.php
+++ index.php
@@ -1,38 +1,16 @@
 <?php
     require_once("modelos/cabecalho.php");
-    require_once("objetos/participantes.php");
-    require_once("objetos/turma.php");
+    //require_once("objetos/participantes.php");
+    //require_once("objetos/turma.php");
     //verificaUsuario();
-    if (isset($_POST["txtPesquisa"])) {
-        $chave = $_POST["txtPesquisa"];
-    } else {
-        $chave = "";
-    }
+    //if (isset($_POST["txtPesquisa"])) {
+      //  $chave = $_POST["txtPesquisa"];
+    //} else {
+      //  $chave = "";
+    //}
 ?>
     <section class="container">
         <header></header>
-        <div>
-            <img src="<?= $_SESSION["foto"] ?>" class="thumbnail" width="140" height="140">
-            <a href="cadastro.php?operacao=2">Editar</a>
-            <!--<a href="cadastro.php">Encerrar</a>-->
-        </div>
-        <br>
-        <!--<form action="" method="post">
-            <input type="text" name="txtPesquisa" class="form-control" placeholder="informe o nome do participante">
-            <br>
-            <button type="submit" class="btn btn-primary">Pesquisar</button>
-        </form>
-        <hr>
-        <?php
-            $participantes = new turma();
-            $objeto = new participantes("", "", "", "", "", "", "");
-            $lista = $objeto->listaAlunos($chave);
-            foreach($lista as $item) {
-                $participante = new participantes($item["arquivoFoto"], "", "", "", $item["login"], $item["nomeCompleto"], "");
-                $participantes->addParticipante($participante);
-            }
-            $participantes->mostrar();
-        ?>-->
     </section>
 <?php
     require_once("modelos/rodape.php");
