5b2ce1927bb4a2b8a88cf121c91e9fb52484b8e7
NilsonTeodoro <nilsonkz22@gmail.com>
2015-05-03 21:33:47 -0300

    Identifica erro

--- negocio/logar.php
+++ negocio/logar.php
@@ -1,33 +1,37 @@
 <?php
-    require_once("objetos/participantes.php");
+	require_once("objetos/participantes.php");
 	session_start();
+	
+	try {
+		echo "nilson";
+		if (isset($_POST["lembrar"])) {
+		   $lembrar = $_POST["lembrar"];
+		   if($lembrar == "lmbUsuario")
+		   {
+			   setcookie("login", utf8_encode(htmlspecialchars($_POST["txtUsuario"])), mktime(0,0,0,12,31,2014));
+		   }
+		}
 
-	echo "nilson";
-    if (isset($_POST["lembrar"])) {
-       $lembrar = $_POST["lembrar"];
-       if($lembrar == "lmbUsuario")
-       {
-           setcookie("login", utf8_encode(htmlspecialchars($_POST["txtUsuario"])), mktime(0,0,0,12,31,2014));
-       }
+		$participante = new participantes("",
+										  "",
+										  "",
+										  "",
+										  utf8_encode(htmlspecialchars($_POST["txtUsuario"])),
+										  "",
+										  utf8_encode(md5(htmlspecialchars($_POST["txtSenha"]))));
+		$objeto = $participante->logar($participante);
+		if ($objeto <> 0){
+			foreach($objeto as $item) {
+				$_SESSION["foto"] = $item["arquivoFoto"];
+			}
+			$_SESSION["login"] = $participante->getLogin();
+			header("Location:../index.php");
+		}else {
+			$_SESSION["negado"] = "Usuário ou senha inválido.";
+			header("Location:../acesso.php");
+		}
+		die();
+	} catch(Exception $e) {
+        echo "erro - ".$e;
     }
-
-    $participante = new participantes("",
-                                      "",
-                                      "",
-                                      "",
-                                      utf8_encode(htmlspecialchars($_POST["txtUsuario"])),
-                                      "",
-                                      utf8_encode(md5(htmlspecialchars($_POST["txtSenha"]))));
-    $objeto = $participante->logar($participante);
-    if ($objeto <> 0){
-        foreach($objeto as $item) {
-            $_SESSION["foto"] = $item["arquivoFoto"];
-        }
-        $_SESSION["login"] = $participante->getLogin();
-        header("Location:../index.php");
-    }else {
-        $_SESSION["negado"] = "Usuário ou senha inválido.";
-        header("Location:../acesso.php");
-    }
-    die();
 ?>
