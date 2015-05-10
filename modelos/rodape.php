9ecc5aa279a54573211ffbe92b82e5e2e21157e9
NilsonTeodoro <nilsonkz22@gmail.com>
2015-05-10 00:17:51 -0300

    10/05/2015 00:17

---
+++ modelos/rodape.php
@@ -0,0 +1,7 @@
+        <footer class="footer" role="contentinfo" data-role="fixed">
+            <div class="container">
+                
+            </div>
+        </footer>
+    </body>
+</html>
