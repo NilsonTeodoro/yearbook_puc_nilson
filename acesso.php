3fd195920d3d75c1ce64e0f41bf2ac45ca2bf50f
NilsonTeodoro <nilsonkz22@gmail.com>
2015-05-10 16:23:46 -0300

    Versão Produção

--- acesso.php
+++ acesso.php
@@ -8,30 +8,29 @@
     }
 ?>
     <section class="container">
-        <h2>Acesso</h2>
         <?php if (isset($_SESSION["negado"])){ ?>
             <p class="text-danger"><?= $_SESSION["negado"] ?></p>
         <?php limpaMensagens(); } ?>
-        <form action="negocio/logar.php" method="post" role="form">
-            <table class="table">
-                <tr>
-                    <td><label for="lblUsuario" class="control-label">Usuário</label></td>
-                    <td><input type="text" name="txtUsuario" class="form-control" value="<?= $usuario ?>"></td>
-                </tr>
-                <tr>
-                    <td><label for="lblSenha"class="control-label">Senha</label></td>
-                    <td><input type="password" name="txtSenha" class="form-control"></td>
-                </tr>
-                <tr>
-                    <td colspan="2">
+        <div class="row">
+        <form action="negocio/logar.php" method="post" class="form-signin" role="form">
+            <div class="col-sm-4"></div>
+            <div class="col-sm-4">
+                <div class="form-signin">
+                    <fieldset>
+                        <legend>Área retrita</legend>
+                        <input type="text" name="txtUsuario" class="form-control" value="<?= $usuario ?>" placeholder="usuário">
+                        <input type="password" name="txtSenha" class="form-control" placeholder="senha">
                         <label class="checkbox"><input type="checkbox" name="lembrar" value="lmbUsuario"> Lembrar</label>
                         <button class="btn btn-lg btn-primary" type="submit">Acessar</button>
                         <a href="cadastro.php">Novo Cadastro</a>
-                    </td>
-                </tr>
-            </table>
+                    </fieldset>
+                </div>
+            </div>
+            <div class="col-sm-4"></div>
         </form>
+        </div>
     </section>
 <?php
     require_once("modelos/rodape.php");
 ?>
+
